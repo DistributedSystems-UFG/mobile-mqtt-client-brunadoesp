082430646fffc2bde721f80aee39bbc23b5e183f
Bruna do Espirito Santo Sousa <brunadoesp@discente.ufg.br>
2023-12-10 14:59:46 -0300

    Alterações Basic Android MQTT Client
    Switch para uma lâmpada

--- app/src/main/java/com/example/basicandroidmqttclient/MainActivity.java
+++ app/src/main/java/com/example/basicandroidmqttclient/MainActivity.java
@@ -6,7 +6,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.hivemq.client.mqtt.datatypes.MqttQos;
@@ -14,13 +16,17 @@ import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5Client;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.UUID;
+
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.basicandroidmqttclient.MESSAGE";
-    public static final String brokerURI = "3.223.10.115";
+    public static final String brokerURI = "3.220.83.84";
 
     Activity thisActivity;
+    ListView listView;
+    ArrayAdapter<String> adapter;
     TextView subMsgTextView;
 
     @Override
@@ -29,7 +35,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         thisActivity = this;
-        subMsgTextView = (TextView) findViewById(R.id.editTextMultiLineSubMsg);
+        listView = findViewById(R.id.minhaLista);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<>());
+        listView.setAdapter(adapter);
     }
 
     /** Called when the user taps the Send button */
@@ -71,13 +79,52 @@ public class MainActivity extends AppCompatActivity {
                 .topicFilter(topicName.getText().toString())
                 .qos(MqttQos.AT_LEAST_ONCE)
                 .callback(msg -> {
-                    thisActivity.runOnUiThread(new Runnable() {
-                        public void run() {
-                            subMsgTextView.setText(new String(msg.getPayloadAsBytes(), StandardCharsets.UTF_8));
-                        }
-                    });
+                    thisActivity.runOnUiThread(() -> {
+                        String message = new String(msg.getPayloadAsBytes(), StandardCharsets.UTF_8);
+                        adapter.insert(message, 0);
+                    });;
                 })
                 .send();
+    }
+
+    public void lightON(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        String topicName = "light";
+        String value = "on";
+
+        Mqtt5BlockingClient client = Mqtt5Client.builder()
+                .identifier(UUID.randomUUID().toString())
+                .serverHost(brokerURI)
+                .buildBlocking();
+
+        client.connect();
+        client.publishWith()
+                .topic(topicName)
+                .qos(MqttQos.AT_LEAST_ONCE)
+                .payload(value.getBytes())
+                .send();
+        client.disconnect();
+
+    }
+
+    public void lightOFF(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        String topicName = "light";
+        String value = "off";
+
+        Mqtt5BlockingClient client = Mqtt5Client.builder()
+                .identifier(UUID.randomUUID().toString())
+                .serverHost(brokerURI)
+                .buildBlocking();
+
+        client.connect();
+        client.publishWith()
+                .topic(topicName)
+                .qos(MqttQos.AT_LEAST_ONCE)
+                .payload(value.getBytes())
+                .send();
+        client.disconnect();
+
     }
 
 
